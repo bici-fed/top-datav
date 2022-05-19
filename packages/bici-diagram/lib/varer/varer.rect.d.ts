6e441e426b276277a19d854952d0be64452daefa
jufeng <jufeng@bicitech.cn>
2022-05-19 09:19:35 +0800

    fix: index.ts一如到src

--- packages/bici-diagram/lib/varer/varer.rect.d.ts
+++
@@ -1,3 +0,0 @@
-import { Node } from '@top-datav/core';
-export declare function biciVarerIconRect(node: Node): void;
-export declare function biciVarerTextRect(node: Node): void;
---
+++ packages/core/src/index.ts
@@ -0,0 +1,5 @@
+export * from './core';
+export * from './options';
+export * from './utils';
+export * from './models';
+export * from './middles';
