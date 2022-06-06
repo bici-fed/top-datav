47f7f7ced25ee44e56d3238bd8a5a43f2e9e4b1c
jufeng <jufeng@bicitech.cn>
2022-06-06 11:08:37 +0800

    fix: 修改packages.json

--- packages/editor/src/index.ts
+++ packages/editor/src/index.ts
@@ -2,5 +2,4 @@
 import 'antd/dist/antd.less';
 export { default as EditorLayout } from './components/Layout';
 export { default as Preview } from './components/Preview';
-
 // https://github.com/umijs/father/issues/48
